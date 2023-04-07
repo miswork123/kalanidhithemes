d93bf6d3dcb84f30696c5ff9b970e9be250f350d
caonguyen <caonguyenvhit@gmail.com>
2023-04-08 04:35:58 +0700

    new section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,16 @@
 // Hide Header on on scroll down
 const header = document.querySelector(".main-header");
+const headerBottum = document.querySelector(".main-header-botom")
 const toggleClass = "is-sticky";
 
 window.addEventListener("scroll", () => {
 	const currentScroll = window.pageYOffset;
 	if (currentScroll > 50) {
 		header.classList.add(toggleClass);
+		headerBottum.classList.add(toggleClass);
 	} else {
 		header.classList.remove(toggleClass);
+		headerBottum.classList.remove(toggleClass);
 	}
 });
 
